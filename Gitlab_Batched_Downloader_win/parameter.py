e0b6492cde6b2ab1d105a6f8f3def5cdb6bbb51a
hongbo ma <mahongbothu@gmail.com>
2025-10-12 12:11:06 +0800

    fix: save token on windows

--- Gitlab_Batched_Downloader_win/parameter.py
+++ Gitlab_Batched_Downloader_win/parameter.py
@@ -1,2 +1,14 @@
-CONFIG_FILE = "token.json"
+import os 
+
+def get_config_path():
+    app_dir = ""
+    if os.name == "nt":  # Windows
+        app_dir = os.path.join(os.getenv("APPDATA"), "GitDownloader")
+    else:  # macOS / Linux
+        app_dir = os.path.join(os.path.expanduser("~"), ".git_downloader")
+    os.makedirs(app_dir, exist_ok=True)
+    return os.path.join(app_dir, "token.json")
+
+CONFIG_FILE = get_config_path()
+
 GITLAB_API = "https://git.tsinghua.edu.cn/api/v4/projects/38341/repository/archive.tar.gz"
